1e0f09c3cd3c7f6f2b3aa3cc42d4cc7c3454436c
km3140 <kimn3140@naver.com>
2023-03-08 22:15:16 +0900

    login-page

--- 01_boilerplate/client/src/components/views/LandingPage/LandingPage.js
+++ 01_boilerplate/client/src/components/views/LandingPage/LandingPage.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 
 const LandingPage = () => {
   useEffect(() => {
-    // 엔드포인트 /api/hello로 get 요청을 보냄 -> 받은 data를 console.log
+    // 엔드포인트 '/api/hello'로 get 요청을 보냄 -> 받은 data를 console.log
     axios.get('/api/hello').then(response => console.log(response.data));
   }, []);
 
-  return <div>LandingPage</div>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+      <h2>시작페이지</h2>
+    </div>
+  );
 };
 
 export default LandingPage;
--- 01_boilerplate/client/src/components/views/LoginPage/LoginPage.js
+++ 01_boilerplate/client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function LoginPage() {
-  return <div>LoginPage</div>;
-}
+const LoginPage = () => {
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
+
+  const onEmailHandler = event => {
+    // 타이핑 할때마다 현재입력값으로 state를 바꿔줌
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = event => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = event => {};
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh' }}>
+      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
